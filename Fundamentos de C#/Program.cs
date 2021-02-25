0eaaef1200201b55aef37bc221a22d2ae4e5baef
knarfortiz <o.g.knarf@hotmail.com>
2021-02-24 20:01:07 -0500

    Inicio curso Fundamentos C#

---
+++ Fundamentos de C#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fundamentos_de_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
